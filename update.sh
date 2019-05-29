84e2f0126e2f9cffe2ef8dd8ec433dde9308d29e
hazelcedar <hazelcedar@gmail.com>
2019-05-29 12:51:26 -0500

    update perm

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ echo "the concourse repository was updated on $(date)" >> concourseLog.txt
 
 git config --global user.email "hazelcedar@gmail.com"
 git config --global user.name "hazelcedar"
+git config --global github.token "1d041fe1be2106c361f68ee6350549e40235a42b"
 
 git add .
 git commit -m "log file update"
