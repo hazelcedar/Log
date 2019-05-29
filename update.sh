6969633c55664226ada08e6178ab4f25617f18da
hazelcedar <hazelcedar@gmail.com>
2019-05-29 13:38:27 -0500

    change

--- update.sh
+++ update.sh
@@ -7,8 +7,6 @@ git clone repo-concourse updated-log
 
 cd updated-log
 
-git pull
-
 echo "the concourse repository was updated on $(date)" >> concourseLog.txt
 
 git config --global user.email "hazelcedar@gmail.com"
@@ -17,4 +15,3 @@ git config --global github.token "1d041fe1be2106c361f68ee6350549e40235a42b"
 
 git add .
 git commit -m "log file update"
-git push
